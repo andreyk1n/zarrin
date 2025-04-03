bc1e8225da67e6d777c4f708702c47183ed50def
andreyk1n <betas23wr@gmail.com>
2025-04-03 21:03:18 +0300

    Add cards section

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,8 +1,10 @@
+import Cards from "../Components/Cards/Cards";
 import Cta from "../Components/Cta/Cta";
 
 const Contacts = () => {
     return (
         <>
+        <Cards />
         <Cta />
         </>
     );
