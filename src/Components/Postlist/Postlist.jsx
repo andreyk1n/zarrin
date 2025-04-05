3d9f50b1d40b7bca5b5d726ed26265fd4c9ab1f2
andreyk1n <betas23wr@gmail.com>
2025-04-05 14:02:26 +0300

    update postList, posts.json

--- src/Components/Postlist/Postlist.jsx
+++ src/Components/Postlist/Postlist.jsx
@@ -1,34 +1,46 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./Postlist.css";
-
 const PostList = () => {
-    const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
 
-    useEffect(() => {
-        fetch("/posts.json")
-            .then((res) => res.json())
-            .then((data) => setPosts(data))
-            .catch((error) => console.error("Помилка завантаження постів:", error));
-    }, []);
+  useEffect(() => {
+    fetch("/posts.json")
+      .then((res) => res.json())
+      .then((data) => setPosts(data))
+      .catch((error) => console.error("Помилка завантаження постів:", error));
+  }, []);
 
-    return (
-        <div className="post-list">
-            {posts.map((post) => (
-                <div key={post.id} className="post">
-                    <img src={post.image} alt={post.title} className="post__image" />
-                    <div className="post__content">
-                        <h2>
-                            <Link to={`/blog/${post.slug}`}>{post.title}</Link>
-                        </h2>
-                        <p className="post__category">{post.category}</p>
-                        <p className="post__date">{post.date}</p>
-                        <p className="post__description">{post.describe}</p>
-                    </div>
-                </div>
-            ))}
-        </div>
-    );
+  const filteredPosts = posts.filter((post) => post.pinned === "false");
+
+  return (
+    <div className="post-list">
+      <div className="post-list__container">
+        {filteredPosts.map((post) => (
+          <div key={post.id} className="post">
+            <Link to={`/blog/${post.slug}`} className="post__image-link">
+              <img src={post.image} alt={post.title} className="post__image" />
+            </Link>
+
+            <div className="post__head">
+              <p className="post__category">{post.category}</p>
+              <p className="post__date">{post.date}</p>
+            </div>
+
+            <div className="post__content">
+              <h3 className="post__title">
+                <Link to={`/blog/${post.slug}`}>{post.title}</Link>
+              </h3>
+              <p className="post__description">{post.describe}</p>
+              <Link to={`/blog/${post.slug}`} className="post__read-more">
+                Read more..
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default PostList;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import Cta from "../Components/Cta/Cta";
 import Hero from "../Components/Hero/Hero";
+import PostList from "../Components/Postlist/Postlist";
 
 const Home = () => {
     return (
         <div>
             <Hero />
+            <PostList />
             <Cta />
         </div>
     );
